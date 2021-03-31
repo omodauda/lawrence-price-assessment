caea82e8142450050de1d559c14b5bf449549e38
omodauda <omodauda.dl@gmail.com>
2021-03-31 18:56:57 +0100

    register user validator

---
+++ src/validators/index.js
@@ -0,0 +1,69 @@
+import Joi from 'joi';
+import { errorMsg } from '../utils/response';
+
+// eslint-disable-next-line consistent-return
+const validateBody = (schema) => (req, res, next) => {
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    const error = result.error.details[0].message;
+    return errorMsg(res, 400, error);
+  }
+
+  if (!req.value) { req.value = {}; }
+  req.value.body = result.value;
+  next();
+};
+
+const schemas = {
+  registerUser: Joi.object().keys({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uk', 'co'] } })
+      .min(5)
+      .max(100)
+      .required()
+      .messages({
+        'string.empty': 'email cannot be an empty field',
+        'string.email': 'please enter a valid email',
+        'any.required': 'email is required',
+      }),
+    password: Joi.string()
+      .min(5)
+      .max(20)
+      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+      .required()
+      .messages({
+        'any.required': 'password is required',
+        'string.pattern.base': 'password must contain only alphanumeric characters',
+        'string.empty': 'password cannot be an empty field',
+        'string.min': 'password should have a minimum length of 5',
+      }),
+  }),
+
+  loginUser: Joi.object().keys({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uk', 'co'] } })
+      .required()
+      .messages({
+        'string.empty': 'email cannot be an empty field',
+        'string.email': 'please enter a valid email',
+        'any.required': 'email is required',
+      }),
+    password: Joi.string()
+      .min(5)
+      .max(20)
+      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+      .required()
+      .messages({
+        'any.required': 'password is required',
+        'string.pattern.base': 'password must contain only alphanumeric characters',
+        'string.empty': 'password cannot be an empty field',
+        'string.min': 'password should have a minimum length of 5',
+      }),
+  }),
+};
+
+export {
+  validateBody,
+  schemas,
+};
